20270d4caf843283ab79cba80ddb763b944fe35e
Cardamced <120644423+Cardamced@users.noreply.github.com>
2024-08-08 15:48:09 +0200

    Style & useForm

--- frontend/src/AncestorNewForm.tsx
+++ frontend/src/AncestorNewForm.tsx
@@ -1,27 +1,103 @@
-import './AncestorNewForm.css';
+import * as React from "react";
+import { useForm } from "react-hook-form";
+import { Ancestor } from "./types/types";
+import "./AncestorNewForm.css";
 
-export default function createNewAncestor() {
-    return (
-        <div>
-            <h1>Création d'un nouvel ancêtre</h1>
-            <form>
-                <label className="form-style" htmlFor="lastname">Nom</label>
-                <input type="text" id="lastname" name="lastname" />
-                <label className="form-style" htmlFor="firstname">Prénom</label>
-                <input type="text" id="firstname" name="firstname" />
-                <label className="form-style" htmlFor="birthdate">Date de naissance</label>
-                <input type="date" id="birthdate" name="birthdate" />
-                <label className="form-style" htmlFor="birth_place">Lieu de naissance</label>
-                <input type="text" id="birth_place" name="birth_place" />
-                <label className="form-style" htmlFor="wedding_date">Date de mariage</label>
-                <input type="date" id="wedding_date" name="wedding_date" />
-                <label className="form-style" htmlFor="wedding_place">Lieu de mariage</label>
-                <input type="text" id="wedding_place" name="wedding_place" />
-                <label className="form-style" htmlFor="death_date">Date de décès</label>
-                <input type="date" id="death_date" name="death_date" />
-                <label className="form-style" htmlFor="death_place">Lieu de décès</label>
-                <input type="text" id="death_place" name="death_place" />
-            </form>
+// on importe le type Ancestor pour le formulaire.
+// Cela évite de réécrire tous les types des propriétés du formulaire.
+
+type FormData = Ancestor;
+
+export default function CreateNewAncestor2() {
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit = async (data: FormData) => {
+    try {
+      const response = await fetch(`https://api.localhost:3009/ancestors`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (!response.ok) {
+        throw new Error("Erreur lors de l'enregistrement des données");
+      }
+
+      const result = await response.json();
+      console.log("enregistrement réussi :", result);
+      // TODO : ajouter ici une modale indiquant le succès de l'opération
+    } catch (error) {
+      console.error("Erreur", error);
+      // TODO : ajouter un message d'erreur à l'utilisateur.
+    }
+  };
+
+  return (
+    <>
+      <h1>Création d'un nouvel ancêtre</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="input-container">
+          <label className="label-style">Nom</label>
+          <input className="input-style" {...register("lastname")} />
         </div>
-    )
+        <div className="input-container">
+          <label className="label-style">Prénom</label>
+          <input className="input-style" {...register("firstname")} />
+        </div>
+        <div className="input-container">
+          <label className="label-style">Image</label>
+          <input className="input-style" {...register("image")} />
+        </div>
+        <div className="input-container">
+          <label className="label-style">Date de naissance</label>
+          <input className="input-style" {...register("birthdate")} />
+        </div>
+        <div className="input-container">
+          <label className="label-style">Lieu de naissance</label>
+          <input className="input-style" {...register("birth_place")} />
+        </div>
+        <div className="input-container">
+          <label className="label-style">Date de mariage</label>
+          <input className="input-style" {...register("wedding_date")} />
+        </div>
+        <div className="input-container">
+          <label className="label-style">Lieu de mariage</label>
+          <input className="input-style" {...register("wedding_place")} />
+        </div>
+        <div className="input-container">
+          <label className="label-style">Date de décès</label>
+          <input className="input-style" {...register("death_date")} />
+        </div>
+        <div className="input-container">
+          <label className="label-style">Lieu de décès</label>
+          <input className="input-style" {...register("death_place")} />
+        </div>
+        <div className="input-container">
+          <label className="label-style">Genre</label>
+          <input className="input-style" {...register("gender")} />
+        </div>
+        {/* <div className="input-container">
+        <label className="label-style">sosa</label>
+        <input className="input-style" {...register("sosa")} />
+      </div> */}
+        <div className="input-container">
+          <label className="label-style">Métier</label>
+          <input className="input-style" {...register("occupation")} />
+        </div>
+        <button
+          type="submit"
+          onClick={() => {
+          }}
+        >
+          Enregistrer
+        </button>
+      </form>
+    </>
+  );
 }
